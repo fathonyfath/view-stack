edd9b2419f51a02663754ce65f33d845528dd62a
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2021-07-17 22:32:50 +0700

    checkpoint on updating the samples

---
+++ app/src/main/java/id/thony/viewstack/sample/TitleActivity.kt
@@ -0,0 +1,12 @@
+package id.thony.viewstack.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TitleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
