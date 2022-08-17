1c87b3c5f3409dc079b7e48cc742e4af83cbec12
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2022-08-17 19:26:44 +0700

    Fix animation behavior

--- app/src/main/java/id/thony/viewstack/sample/ToolbarMultipleBackstackHandler.kt
+++ app/src/main/java/id/thony/viewstack/sample/ToolbarMultipleBackstackHandler.kt
@@ -1,10 +1,14 @@
 package id.thony.viewstack.sample
 
 import android.graphics.drawable.Drawable
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.core.view.forEach
 import id.thony.viewstack.Backstack
 import id.thony.viewstack.DefaultBackstackHandler
 import id.thony.viewstack.NavigationCommand
@@ -25,16 +29,93 @@ class ToolbarMultipleBackstackHandler(
 
         when (command) {
             NavigationCommand.Replace -> {
-                val topView = this.container.getChildAt(0)
+                val oldStackTop = oldStack.peekKey()
+                val topView = this.container.findViewById<View>(oldStackTop.hashCode())
                 if (topView != null) {
                     val viewKey = getViewKey(topView)
                     saveViewState(topView, oldStack.obtainViewState(viewKey))
                 }
 
-                super.handleBackstackChange(navigator, oldStack, newStack, command)
+                if (this.container.childCount == 1) {
+                    val anim = createFadeOutAnimation(topView, this.container)
+                    topView.startAnimation(anim)
+                } else {
+                    this.container.forEach { it.clearAnimation() }
+                    this.container.removeAllViews()
+                }
+
+                val upcomingKey = newStack.peekKey()
+                val upcomingViewState = newStack.obtainViewState(upcomingKey)
+                val view = buildView(upcomingKey).apply {
+                    id = upcomingKey.hashCode()
+                }
+                restoreViewState(view, upcomingViewState)
+
+                container.addView(view)
+                view.startAnimation(createFadeInAnimation())
             }
-            NavigationCommand.Restore, NavigationCommand.Push, NavigationCommand.Pop -> {
-                super.handleBackstackChange(navigator, oldStack, newStack, command)
+            NavigationCommand.Restore -> {
+                this.container.removeAllViews()
+
+                val upcomingKey = newStack.peekKey()
+                val upcomingViewState = newStack.obtainViewState(upcomingKey)
+                val view = buildView(upcomingKey).apply {
+                    id = upcomingKey.hashCode()
+                }
+                restoreViewState(view, upcomingViewState)
+
+                container.addView(view)
+            }
+            NavigationCommand.Push -> {
+                if (oldStack.peekKey() === newStack.peekKey()) return
+
+                val oldView = this.container.findViewById<View>(oldStack.peekKey().hashCode())
+                requireNotNull(oldView)
+                val viewKey = getViewKey(oldView)
+                saveViewState(oldView, oldStack.obtainViewState(viewKey))
+
+                if (this.container.childCount == 1) {
+                    val anim = createPushSlideOutAnimation(oldView, this.container)
+                    oldView.startAnimation(anim)
+                } else {
+                    this.container.forEach { it.clearAnimation() }
+                    this.container.removeAllViews()
+                }
+
+                val upcomingKey = newStack.peekKey()
+                val upcomingViewState = newStack.obtainViewState(upcomingKey)
+                val view = buildView(upcomingKey).apply {
+                    id = upcomingKey.hashCode()
+                }
+                restoreViewState(view, upcomingViewState)
+
+                container.addView(view)
+                view.startAnimation(createPushSlideInAnimation())
+            }
+            NavigationCommand.Pop -> {
+                if (oldStack.peekKey() === newStack.peekKey()) return
+
+                val oldView = this.container.findViewById<View>(oldStack.peekKey().hashCode())
+                requireNotNull(oldView)
+
+                if (this.container.childCount == 1) {
+                    val anim = createPopSlideOutAnimation(oldView, this.container)
+                    oldView.startAnimation(anim)
+                } else {
+                    this.container.forEach { it.clearAnimation() }
+                    this.container.removeAllViews()
+                }
+
+
+                val upcomingKey = newStack.peekKey()
+                val upcomingViewState = newStack.obtainViewState(upcomingKey)
+                val view = buildView(upcomingKey).apply {
+                    id = upcomingKey.hashCode()
+                }
+                restoreViewState(view, upcomingViewState)
+
+                container.addView(view)
+                view.startAnimation(createPopSlideInAnimation())
             }
         }
     }
@@ -65,5 +146,59 @@ class ToolbarMultipleBackstackHandler(
             activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
             activity.drawerToggleDelegate?.setActionBarDescription(contentDescription)
         }
+    }
+
+    private fun createFadeOutAnimation(view: View, container: ViewGroup): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.fade_out).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(anim: Animation) = Unit
+
+                override fun onAnimationEnd(anim: Animation) {
+                    container.removeView(view)
+                }
+
+                override fun onAnimationRepeat(anim: Animation) = Unit
+            })
+        }
+    }
+
+    private fun createFadeInAnimation(): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.fade_in)
+    }
+
+    private fun createPushSlideOutAnimation(view: View, container: ViewGroup): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.push_slide_out).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(anim: Animation) = Unit
+
+                override fun onAnimationEnd(anim: Animation) {
+                    container.removeView(view)
+                }
+
+                override fun onAnimationRepeat(anim: Animation) = Unit
+            })
+        }
+    }
+
+    private fun createPushSlideInAnimation(): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.push_slide_in)
+    }
+
+    private fun createPopSlideOutAnimation(view: View, container: ViewGroup): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.pop_slide_out).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(anim: Animation) = Unit
+
+                override fun onAnimationEnd(anim: Animation) {
+                    container.removeView(view)
+                }
+
+                override fun onAnimationRepeat(anim: Animation) = Unit
+            })
+        }
+    }
+
+    private fun createPopSlideInAnimation(): Animation {
+        return AnimationUtils.loadAnimation(activity, R.anim.pop_slide_in)
     }
 }
