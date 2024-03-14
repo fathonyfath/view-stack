eb9e2bc0d14e07fe70e65545a5056be89a1744f7
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2024-03-14 23:08:59 +0900

    Upgrade to AGP 8.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "7.4.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
