a497a9b2621a71e6691d901895fce375f9a61666
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2024-03-14 23:21:56 +0900

    Modernize build script

---
+++ library/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "id.thony.viewstack"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 5
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:21.0.1")
+}
